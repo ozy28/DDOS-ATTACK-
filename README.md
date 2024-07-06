0fd2782ce9e7aaaa7cdc922ad5844d4ab96613bb
ozy28 <fauzinurramadhan565@gmail.com>
2024-07-06 12:41:32 +0700

    Update and rename README.md to SERANGAN-DDOS-.py

--- README.md
+++
@@ -1,5 +0,0 @@
-# DDOS-ATTACK-
-ddos via termux by ozy
-ddos attack merupakan serangan berturut turut sampai server down. dan untuk cara gunakan nya simpel sekali. 
-lihat dibawah ini !!
-
---
+++ SERANGAN-DDOS-.py
@@ -0,0 +1,14 @@
+# DDOS-ATTACK-
+ddos via termux by ozy
+ddos attack merupakan serangan berturut turut sampai server down. dan untuk cara gunakan nya simpel sekali. 
+lihat dibawah ini !!
+
+apt update && apt upgrade 
+pkg install python
+pkg install python3
+pkg install git
+git clone https://github.com/ozy28/SERANGAN-DDOS-
+cd SERANGAN-DDOS
+SERANGAN-DDOS.py
+python3 (masukan ip) (masukan port 80) masukan (135)
+gunakan dengan bijak
